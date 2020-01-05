8aec4834533a0233c8f29aa87b4926cd8ba6d557
Aymen Ayadi <ayadi_aymen@outlook.com>
2020-01-05 17:13:37 +0100

    frontend structure

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export class App extends Component {
+  render() {
+    return;
+    <Router>
+      <Switch>
+        <Route exact path='/' render={() => <div>home page</div>} />
+      </Switch>
+    </Router>;
+  }
+}
+
+export default App;
